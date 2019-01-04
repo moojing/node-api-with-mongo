933c3eeb7f3db4729e505598f878870ab09f512b
MuJing Tsai <mujingtsai@Mu-Jing.local>
2019-01-04 10:36:24 +0800

    add news schema

--- model/News.js
+++ model/News.js
@@ -1,8 +1,36 @@
 const mongoose = require('mongoose')
 const Schema = require('mongoose').Schema
+const autoIncrement = require('./mongoConnect').autoIncrement
 
-const NewsSchema =  { name: String }
 
+const NewsSchema =  new Schema({ 
+    img1:String,
+    img2:String,
+    i18n:{
+        "zh-cn":{
+            title:"",
+            description:"",
+            
+        },
+        "en-us":{
+            title:"",
+            description:""
+        },
+        "it-ch":{
+            title:"",
+            description:""
+        },
+        "ja-jp":{
+            title:"",
+            description:""
+        },
+        "es-es":{
+            title:"",
+            description:""
+        }
+    } 
+})
+NewsSchema.plugin(autoIncrement.plugin, 'News');
 const News = mongoose.model('News', new Schema(NewsSchema))
 
 module.exports = News
